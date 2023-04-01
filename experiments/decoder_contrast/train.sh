eed2184271770ba40d73594e3f68593cdf0a05aa
JDekun <jurry_k@outlook.com>
2023-04-01 20:08:35 +0800

    本地化成功

--- experiments/decoder_contrast/train.sh
+++ experiments/decoder_contrast/train.sh
@@ -3,11 +3,11 @@ now=$(date +"%Y%m%d_%H%M%S")
 PARTITION=$1
 JOB_NAME=$2
 ROOT=../..
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=3,4
 export PYTHONPATH=$ROOT:$PYTHONPATH
 
 
-python -m torch.distributed.launch --nproc_per_node=1 --master_port 29522 ../../train_contrast.py --config=config.yaml  2>&1 | tee log_$now.log
+python -m torch.distributed.launch --nproc_per_node=2 --master_port 29523 ../../train_contrast.py --config=config.yaml  2>&1 | tee log_$now.log
 # mkdir -p checkpoints/result
 # python ../../eval.py --base_size=512 --scales 1.0 --config=config.yaml --model_path=checkpoints/best.pth --save_folder=checkpoints/result/ 2>&1 | tee checkpoints/result/eva_$now.log
 # python ../../eval.py --base_size=512 --scales 1.0 --config=config.yaml --model_path=checkpoints/epoch_99.pth --save_folder=checkpoints/result/ 2>&1 | tee checkpoints/result/eva_99_$now.log
