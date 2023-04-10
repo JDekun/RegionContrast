32e71658c770b9e03fc7e6c776225768b3f36618
JDekun <jurry_k@outlook.com>
2023-04-10 14:40:30 +0800

    de

--- pyseg/models/resnet.py
+++ pyseg/models/resnet.py
@@ -142,10 +142,10 @@ class ResNet(nn.Module):
             conv3x3(64, 64),
             norm_layer(64),
             nn.ReLU(inplace=True),
-            conv3x3(64, 64))
+            conv3x3(64, self.inplanes))
         # self.conv1 =nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
         #                        bias=False)
-        self.bn1 = norm_layer(64)
+        self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True)  # change
 
